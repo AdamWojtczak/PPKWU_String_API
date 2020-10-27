0ce669417c4360f49e2b058b2de4f493a0c6619b
AdamWojtczak <adamwojtczak807@gmail.com>
2020-10-27 14:35:47 +0100

    Checking for lowercase, uppercase, special signs and numerics

--- src/main/java/com/example/demo/StringAnalyzer.java
+++ src/main/java/com/example/demo/StringAnalyzer.java
@@ -11,6 +11,20 @@ public class StringAnalyzer {
 	@RequestMapping(method = RequestMethod.GET)
 	public String reverseString( @RequestParam("stringToBeReversed") String stringToBeAnalyzed) {
 
+		for (int i = 0; i < stringToBeAnalyzed.length(); i++) {
+			char oneChar = stringToBeAnalyzed.charAt(i);
+			int lowerCaseCounter = 0, upperCaseCounter = 0, numericCounter = 0, specialSignCounter = 0;
+			if (isLowerCase(oneChar)) {
+				lowerCaseCounter++;
+			} else if (isUpperCase(oneChar)) {
+				upperCaseCounter++;
+			} else if (isSpecialSign(oneChar)) {
+				specialSignCounter++;
+			} else if (isNumeric(onceChar)) {
+				numericCounter++;
+			}
+		}
+
 		return stringToBeAnalyzed;
 	}
 }
